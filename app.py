f4078912987c4651837be8142f5e39aa96256431
Diego Armando Guzmán <darmandoguzman@ucundinamarca.edu.co>
2026-08-26 13:40:44 -0500

    plantilla base con menu y primeras rutas de Etapa 1

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 # Definir la ruta principal
 @app.route("/")
 def inicio():
-  return render_template("index.html")
-
+  return render_template("inicio.html")
+@app.route("/etapa1/problema")
+def problema():
+  return render_template("etapa1/problema.html")
 
 # Ejecutar el servidor en modo desarrollo
 if __name__ == "__main__":
